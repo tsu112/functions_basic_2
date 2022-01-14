c414db09760b2debd5dd555d0399b522a9948dba
Sue Yang <sueyang112@hotmail.com>
2022-01-13 19:47:14 -0500

    first commit

---
+++ functions_basic_2.py
@@ -0,0 +1,31 @@
+# 1 Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
+# Example: countdown(5) should return [5,4,3,2,1,0]
+
+# count = []
+
+
+# def countdown(num):
+#     for x in range(num, -1, -1):
+#         count.append(x)
+#     return(count)
+
+
+# print(countdown(5))
+############################################################
+
+# 2 Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
+# Example: print_and_return([1,2]) should print 1 and return 2
+
+# def print_and_return(a, b):
+#     print(a)
+#     return(b)
+
+
+# print(print_and_return(1, 2))
+############################################################
+
+# 3 First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
+# Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
+
+def first_plus_length():
+    list
